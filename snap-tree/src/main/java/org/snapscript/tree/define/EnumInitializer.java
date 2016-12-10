b63395f6087a33afd23261e1acc8fb16e94a248d
Niall Gallagher <gallagher_niall@yahoo.com>
2016-12-10 02:24:23 +0000

    Wrap enum values in a proxy

--- snap-tree/src/main/java/org/snapscript/tree/define/EnumInitializer.java
+++ snap-tree/src/main/java/org/snapscript/tree/define/EnumInitializer.java
@@ -7,13 +7,16 @@ import static org.snapscript.core.Reserved.ENUM_VALUES;
 import java.util.List;
 import java.util.concurrent.Callable;
 
+import org.snapscript.core.Context;
 import org.snapscript.core.InternalStateException;
+import org.snapscript.core.Module;
 import org.snapscript.core.Result;
 import org.snapscript.core.ResultType;
 import org.snapscript.core.Scope;
 import org.snapscript.core.State;
 import org.snapscript.core.Type;
 import org.snapscript.core.Value;
+import org.snapscript.core.convert.ProxyWrapper;
 import org.snapscript.core.define.Initializer;
 import org.snapscript.tree.ArgumentList;
 import org.snapscript.tree.NameExtractor;
@@ -36,12 +39,15 @@ public class EnumInitializer extends Initializer {
    public Result execute(Scope scope, Type type) throws Exception {
       String name = extractor.extract(scope);
       State state = scope.getState();
-      
+
       if(type == null) {
          throw new InternalStateException("No type found for enum " + name); // class not found
       }
       Callable<Result> call = binder.bind(scope, type);
-           
+      Module module = scope.getModule();
+      Context context = module.getContext();
+      ProxyWrapper wrapper = context.getWrapper();
+      
       if(call == null){
          throw new InternalStateException("No constructor for enum '" + name + "' in '" + type+ "'");
       }
@@ -49,11 +55,12 @@ public class EnumInitializer extends Initializer {
       Scope instance = result.getValue();
       Value value = state.getValue(ENUM_VALUES);
       List values = value.getValue();
+      Object object = wrapper.toProxy(instance);
       
       initializer.declareConstant(scope, name, type, type, instance);
       initializer.declareConstant(instance, ENUM_NAME, type, name); // might declare name as property many times
       initializer.declareConstant(instance, ENUM_ORDINAL, type, index);
-      values.add(instance);
+      values.add(object);
       
       return ResultType.getNormal(instance);
    }
