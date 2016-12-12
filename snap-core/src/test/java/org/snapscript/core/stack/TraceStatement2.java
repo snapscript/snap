4c32b4f9f23c21b7d98d71735bb53b17a643c912
Niall Gallagher <gallagher_niall@yahoo.com>
2016-12-12 22:59:58 +0000

    Draw out the new trace statement

---
+++ snap-core/src/test/java/org/snapscript/core/stack/TraceStatement2.java
@@ -0,0 +1,43 @@
+package org.snapscript.core.stack;
+
+import org.snapscript.core.Result;
+import org.snapscript.core.Statement;
+import org.snapscript.core.error.ErrorHandler;
+import org.snapscript.core.trace.Trace;
+import org.snapscript.core.trace.TraceInterceptor;
+
+public class TraceStatement2 { // TraceEvaluation does not need any change it does not deal with the stack!!
+   
+   private final TraceInterceptor interceptor;
+   private final ErrorHandler handler;
+   private final Statement statement;
+   private final Trace trace;
+   
+   public TraceStatement2(TraceInterceptor interceptor, ErrorHandler handler, Statement statement, Trace trace) {
+      this.interceptor = interceptor;
+      this.statement = statement;
+      this.handler = handler;
+      this.trace = trace;
+   }
+   
+   public Result compile(Scope2 scope) throws Exception {
+      return null; //statement.compile(scope);
+   }
+   
+   public Result execute(Scope2 scope) throws Exception {
+      State2 stack = scope.getStack();
+      State2 outer = stack.create(); // allow the stack to grow!! 
+      
+      try {
+         //interceptor.before(scope, trace);
+         //return statement.execute(scope); 
+         return null;
+      } catch(Exception cause) {
+         //return handler.throwInternal(scope, cause);
+         return null;
+      } finally {
+         //interceptor.after(scope, trace);
+         outer.clear(); // this clears it up to the initial stack point
+      }
+   }
+}
