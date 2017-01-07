b3e6ce9a7a8bf042c3301ef722234bf28f8c096c
Niall Gallagher <gallagher_niall@yahoo.com>
2017-01-07 02:44:32 +0000

    Merge compound expression

--- snap-tree/src/main/java/org/snapscript/tree/Expression.java
+++ snap-tree/src/main/java/org/snapscript/tree/Expression.java
@@ -1,19 +1,28 @@
 package org.snapscript.tree;
 
 import org.snapscript.core.Evaluation;
+import org.snapscript.core.InternalStateException;
 import org.snapscript.core.Scope;
 import org.snapscript.core.Value;
 
 public class Expression implements Evaluation {
    
-   private final Evaluation evaluation;
+   private final Evaluation[] list;
    
-   public Expression(Evaluation evaluation) {
-      this.evaluation = evaluation;
+   public Expression(Evaluation... list) {
+      this.list = list;
    }
-   
+
    @Override
    public Value evaluate(Scope scope, Object left) throws Exception {
-      return evaluation.evaluate(scope, left);
+      if(list.length <= 0) {
+         throw new InternalStateException("Evaluation is empty");
+      }
+      Value value = list[0].evaluate(scope, left);
+      
+      for(int i = 1; i < list.length; i++){
+         value = list[i].evaluate(scope, left);
+      }
+      return value;
    }
 }
