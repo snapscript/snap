0439604d62e5f78720c75419a50e63e3874f5fa1
Niall Gallagher <gallagher_niall@yahoo.com>
2017-01-21 23:42:30 +0000

    Extract out error builder

---
+++ snap-core/src/main/java/org/snapscript/core/error/InternalErrorBuilder.java
@@ -0,0 +1,41 @@
+package org.snapscript.core.error;
+
+import org.snapscript.core.stack.ThreadStack;
+
+public class InternalErrorBuilder {
+
+   private final ThreadStack stack;
+   private final boolean replace;
+   
+   public InternalErrorBuilder(ThreadStack stack) {
+      this(stack, true);
+   }
+   
+   public InternalErrorBuilder(ThreadStack stack, boolean replace) {
+      this.replace = replace;
+      this.stack = stack;
+   }
+   
+   public InternalError create(Object value) {
+      InternalError error = new InternalError(value);
+      
+      if(replace) {
+         if(Throwable.class.isInstance(value)) {
+            Throwable cause = (Throwable)value;
+            StackTraceElement[] trace = stack.build(cause);
+            
+            if(trace.length > 0) {
+               cause.setStackTrace(trace);
+               error.setStackTrace(trace);
+            }
+         } else {
+            StackTraceElement[] trace = stack.build();
+            
+            if(trace.length > 0) {
+               error.setStackTrace(trace); // when there is no cause
+            }
+         }
+      }
+      return error;
+   }
+}
--- snap-core/src/main/java/org/snapscript/core/error/InternalErrorHandler.java
+++ snap-core/src/main/java/org/snapscript/core/error/InternalErrorHandler.java
@@ -6,38 +6,17 @@ import org.snapscript.core.stack.ThreadStack;
 
 public class InternalErrorHandler {
 
-   private final ThreadStack stack;
-   private final boolean replace;
+   private final InternalErrorBuilder builder;
    
    public InternalErrorHandler(ThreadStack stack) {
       this(stack, true);
    }
    
    public InternalErrorHandler(ThreadStack stack, boolean replace) {
-      this.replace = replace;
-      this.stack = stack;
+      this.builder = new InternalErrorBuilder(stack, replace);
    }
    
    public Result throwInternal(Scope scope, Object value) {
-      InternalError error = new InternalError(value);
-      
-      if(replace) {
-         if(Throwable.class.isInstance(value)) {
-            Throwable cause = (Throwable)value;
-            StackTraceElement[] trace = stack.build(cause);
-            
-            if(trace.length > 0) {
-               cause.setStackTrace(trace);
-               error.setStackTrace(trace);
-            }
-         } else {
-            StackTraceElement[] trace = stack.build();
-            
-            if(trace.length > 0) {
-               error.setStackTrace(trace); // when there is no cause
-            }
-         }
-      }
-      throw error;
+      throw builder.create(value);
    }
 }
