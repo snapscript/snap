55a6734c13bcf0e3cd084935ea5479091db577bb
Niall Gallagher <gallagher_niall@yahoo.com>
2017-01-23 21:35:10 +0000

    Fix performance test

--- snap-compile/src/test/java/org/snapscript/compile/CompilePerformanceTest.java
+++ snap-compile/src/test/java/org/snapscript/compile/CompilePerformanceTest.java
@@ -80,6 +80,7 @@ public class CompilePerformanceTest extends TestCase {
       //compileScript("perf4.js");  
      // compileScript("/script/script13.snap"); 
       compileScript("/perf/perf4.snap"); 
+      compileScript("/perf/perf1.snap");
  /*     executeScript("perf2.js");    
       executeScript("perf3.js"); */   
    }
