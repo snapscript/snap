0d2f029e35e92ce4b76e6ed47a9ee7794023d3d1
Niall Gallagher <gallagher_niall@yahoo.com>
2017-05-14 22:58:58 +0100

    Update reference

--- snap-tree/src/main/java/org/snapscript/tree/function/FunctionReference.java
+++ snap-tree/src/main/java/org/snapscript/tree/function/FunctionReference.java
@@ -12,19 +12,19 @@ import org.snapscript.tree.NameReference;
 public class FunctionReference implements Evaluation {
    
    private final FunctionReferenceBuilder builder;
-   private final NameReference referene;
+   private final NameReference reference;
    private final Evaluation variable;
    
    public FunctionReference(Evaluation variable, Evaluation method) {
       this.builder = new FunctionReferenceBuilder();
-      this.referene = new NameReference(method);
+      this.reference = new NameReference(method);
       this.variable = variable;
    }
 
    @Override
    public Value evaluate(Scope scope, Object left) throws Exception {
       Value value = variable.evaluate(scope, left);
-      String match = referene.getName(scope);
+      String match = reference.getName(scope);
       Module module = scope.getModule(); // is this the correct module?
       Object object = value.getValue();
       Function function = builder.create(module, object, match); 
