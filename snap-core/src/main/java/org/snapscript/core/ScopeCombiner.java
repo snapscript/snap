4203515e5f527573bf8bc66e64a4c35788dc3ae6
Niall Gallagher <gallagher_niall@yahoo.com>
2017-09-08 00:42:10 +0100

    Remove scope combiner

--- snap-core/src/main/java/org/snapscript/core/ScopeCombiner.java
+++
@@ -1,9 +0,0 @@
-package org.snapscript.core;
-
-@Bug("remove me")
-public class ScopeCombiner {
-
-   public static Scope combine(Scope inner, Scope outer) {
-      return new CompoundScope(inner, outer);
-   }
-}
