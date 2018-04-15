070803686d5556376030f9b3cc963c29bac46eac
Niall Gallagher <gallagher_niall@yahoo.com>
2018-04-15 22:31:13 +0100

    Do not throw an exception from checker

--- snap-core/src/main/java/org/snapscript/core/convert/CastChecker.java
+++ snap-core/src/main/java/org/snapscript/core/convert/CastChecker.java
@@ -4,6 +4,7 @@ import static org.snapscript.core.convert.Score.EXACT;
 import static org.snapscript.core.convert.Score.INVALID;
 import static org.snapscript.core.convert.Score.SIMILAR;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -70,19 +71,18 @@ public class CastChecker {
    public Score toFunction(Type actual, Type constraint) throws Exception {
       Category category = constraint.getCategory();
       
-      if(!category.isFunction()) {
-         throw new InternalStateException("Type '" + actual + "' is not a function");
-      }
-      Function possible = finder.findFunctional(actual);
-      List<Function> functions = constraint.getFunctions();
-      
-      if(functions.isEmpty()) {
-         throw new InternalStateException("Type '" + actual + "' does not contain any functions");
-      }
-      Function required = functions.get(0);
-      
-      if(possible != null) {         
-         return comparator.compare(possible, required);         
+      if(category.isFunction()) {
+         Function possible = finder.findFunctional(actual);
+         List<Function> functions = constraint.getFunctions();
+         Iterator<Function> iterator = functions.iterator();
+         
+         if(iterator.hasNext()) {
+            Function required = iterator.next();
+         
+            if(possible != null) {         
+               return comparator.compare(possible, required);         
+            }
+         }
       }
       return INVALID;
    }
@@ -91,19 +91,18 @@ public class CastChecker {
       Type type = extractor.getType(value);
       Category category = type.getCategory();
       
-      if(!category.isFunction()) {
-         throw new InternalStateException("Type '" + type + "' is not a function");
-      }
-      Function possible = finder.findFunctional(type);
-      List<Function> functions = constraint.getFunctions();
-      
-      if(functions.isEmpty()) {
-         throw new InternalStateException("Type '" + actual + "' does not contain any functions");
-      }
-      Function required = functions.get(0);
-      
-      if(possible != null) {
-         return comparator.compare(possible, required);
+      if(category.isFunction()) {
+         Function possible = finder.findFunctional(type);
+         List<Function> functions = constraint.getFunctions();
+         Iterator<Function> iterator = functions.iterator();
+         
+         if(iterator.hasNext()) {
+            Function required = iterator.next();
+            
+            if(possible != null) {
+               return comparator.compare(possible, required);
+            }
+         }
       }
       return INVALID;
    }
