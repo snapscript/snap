26702936f741d413ca5f8b1919600561c9d261b5
Authrus <authrus.root@gmail.com>
2019-01-12 17:16:33 +0000

    Fix super constructor

--- snap-tree/src/main/java/org/snapscript/tree/define/SuperFunctionMatcher.java
+++ snap-tree/src/main/java/org/snapscript/tree/define/SuperFunctionMatcher.java
@@ -1,6 +1,7 @@
 package org.snapscript.tree.define;
 
 import org.snapscript.core.Context;
+import org.snapscript.core.constraint.Constraint;
 import org.snapscript.core.function.bind.FunctionBinder;
 import org.snapscript.core.function.bind.FunctionMatcher;
 import org.snapscript.core.function.dispatch.FunctionDispatcher;
@@ -20,12 +21,31 @@ public class SuperFunctionMatcher {
       this.reference = reference;
       this.type = type;
    }
+   
+   public FunctionDispatcher match(Scope scope, Constraint left) throws Exception {
+      return resolve(scope, left);
+   }
 
    public FunctionDispatcher match(Scope scope, Value left) throws Exception {
       if(dispatcher == null) {
          dispatcher = resolve(scope, left);
       }
       return dispatcher;
+   }
+   
+   private FunctionDispatcher resolve(Scope scope, Constraint left) throws Exception {
+      Class base = type.getType();
+      
+      if(base == null) {
+         Module module = scope.getModule();
+         Context context = module.getContext();
+         FunctionBinder binder = context.getBinder();
+         String name = reference.getName(scope);         
+         FunctionMatcher matcher = binder.bind(name);
+         
+         return matcher.match(scope, left);
+      }
+      return new SuperDispatcher(type);
    }
    
    private FunctionDispatcher resolve(Scope scope, Value left) throws Exception {
--- snap-tree/src/main/java/org/snapscript/tree/define/SuperInvocation.java
+++ snap-tree/src/main/java/org/snapscript/tree/define/SuperInvocation.java
@@ -14,60 +14,49 @@ import org.snapscript.core.type.Type;
 import org.snapscript.core.variable.Value;
 import org.snapscript.tree.ArgumentList;
 import org.snapscript.tree.NameReference;
+import org.snapscript.tree.construct.ConstructArgumentList;
 
 public class SuperInvocation extends Evaluation {
 
+   private final ConstructArgumentList arguments;
    private final SuperInstanceBuilder builder;
    private final CaptureScopeExtractor extractor;
    private final SuperFunctionMatcher matcher;
    private final NameReference reference;
-   private final ArgumentList arguments;
    private final Constraint constraint;
    private final Type type;
    
    public SuperInvocation(Evaluation function, ArgumentList arguments, Type type) {
+      this.arguments = new ConstructArgumentList(arguments);
       this.extractor = new CaptureScopeExtractor(EXECUTE_SUPER);
       this.reference = new NameReference(function);
       this.matcher = new SuperFunctionMatcher(reference, type);
       this.constraint = new StaticConstraint(type);
       this.builder = new SuperInstanceBuilder(type);
-      this.arguments = arguments;
       this.type = type;
    }
 
    @Override
    public Constraint compile(Scope scope, Constraint left) throws Exception {
-      Value value = Value.getTransient(type);
-      FunctionDispatcher dispatcher = matcher.match(scope, value);  
-      
-      if(arguments != null) {
-         Scope outer = scope.getScope();
-         Scope compound = extractor.extract(scope, outer);
-         Type[] list = arguments.compile(compound, type); // arguments have no left hand side
+      Scope outer = scope.getScope();
+      FunctionDispatcher dispatcher = matcher.match(scope, constraint);  
+      Scope compound = extractor.extract(scope, outer);
+      Type[] list = arguments.compile(compound, type); // arguments have no left hand side
 
-         return dispatcher.compile(scope, constraint, list);
-      }
-      return dispatcher.compile(scope, constraint, type);
+      return dispatcher.compile(scope, constraint, list);
    }
    
    @Override
    public Value evaluate(Scope scope, Value left) throws Exception {
       Type real = scope.getType();  
+      Scope outer = real.getScope();
       Scope instance = builder.create(scope, left);
       Value value = Value.getTransient(instance);
+      Scope compound = extractor.extract(scope, outer);
+      Object[] list = arguments.create(compound, real); // arguments have no left hand side
       FunctionDispatcher dispatcher = matcher.match(instance, NULL);  
-      
-      if(arguments != null) {
-         Scope outer = real.getScope();
-         Scope compound = extractor.extract(scope, outer);
-         Object[] list = arguments.create(compound, real); // arguments have no left hand side
-         Connection connection = dispatcher.connect(instance, value, list);
-         Object result = connection.invoke(instance, value, list);
-         
-         return Value.getTransient(result);
-      }
-      Connection connection = dispatcher.connect(instance, value, real);
-      Object result = connection.invoke(instance, value, real);
+      Connection connection = dispatcher.connect(instance, value, list);
+      Object result = connection.invoke(instance, value, list);
       
       return Value.getTransient(result);
    }
