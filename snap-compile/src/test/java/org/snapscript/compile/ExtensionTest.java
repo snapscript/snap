7ff07e4be87bd44b17a82c66483f3d62b375b689
Authrus <authrus.root@gmail.com>
2019-01-13 22:48:06 +0000

    Update tests

--- snap-compile/src/test/java/org/snapscript/compile/ExtensionTest.java
+++ snap-compile/src/test/java/org/snapscript/compile/ExtensionTest.java
@@ -2,7 +2,7 @@ package org.snapscript.compile;
 
 import junit.framework.TestCase;
 
-public class ExtensionTest extends TestCase {
+public class ExtensionTest extends ScriptTestCase {
 
    private static final String SOURCE_1 = 
    "var list = new File(\".\").findFiles(\".*\");\n"+
@@ -14,24 +14,15 @@ public class ExtensionTest extends TestCase {
    private static final String SOURCE_3 = 
    "let x = URL('http://www.google.com').get().response();\n"+
    "println(x);\n"+
-   "println(x);"; 
-   
+   "println(x);";
+
+   private static final String SOURCE_4 =
+   "File(`${System.getProperty('user.home')}/.bash_history`).forEachLine(l -> println(l));\n";
+
    public void testFileExtension() throws Exception {
-      Compiler compiler = ClassPathCompilerBuilder.createCompiler();
-      System.err.println(SOURCE_1);
-      Executable executable = compiler.compile(SOURCE_1);
-      executable.execute();
-   }
-   public void testDateExtension() throws Exception {
-      Compiler compiler = ClassPathCompilerBuilder.createCompiler();
-      System.err.println(SOURCE_2);
-      Executable executable = compiler.compile(SOURCE_2);
-      executable.execute();
-   }  
-   public void testURLExtension() throws Exception {
-      Compiler compiler = ClassPathCompilerBuilder.createCompiler();
-      System.err.println(SOURCE_3);
-      Executable executable = compiler.compile(SOURCE_3);
-      executable.execute();
+      assertScriptExecutes(SOURCE_1);
+      assertScriptExecutes(SOURCE_2);
+      assertScriptExecutes(SOURCE_3);
+      assertScriptExecutes(SOURCE_4);
    }  
 }
