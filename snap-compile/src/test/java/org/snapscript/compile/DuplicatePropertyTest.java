1cfb951de34c540a86d6edc44e6de627d1d31098
Authrus <authrus.root@gmail.com>
2019-01-13 00:52:22 +0000

    Add method reference

---
+++ snap-compile/src/test/java/org/snapscript/compile/DuplicatePropertyTest.java
@@ -0,0 +1,47 @@
+package org.snapscript.compile;
+
+import org.snapscript.compile.staticanalysis.CompileTestCase;
+import org.snapscript.core.Bug;
+
+public class DuplicatePropertyTest extends CompileTestCase {
+
+   private static final String SUCCESS_1 = 
+   "class Foo {\n"+
+   "   private let x = 1;\n"+
+   "}\n"+
+   "class Bar extends Foo {\n"+
+   "   let x = 11;\n"+
+   "}\n";
+   
+   private static final String SUCCESS_2 = 
+   "class Foo {\n"+
+   "   private let x = 1;\n"+
+   "   getName(){\n"+
+   "     return 'Foo';\n"+
+   "   }\n"+
+   "}\n"+
+   "class Bar extends Foo {\n"+
+   "   let x = 11;\n"+
+   "   getName(){\n"+
+   "     return 'Bar';\n"+
+   "   }\n"+   
+   "}\n"+
+   "println(new Bar().name);\n"+
+   "assert new Bar().name == 'Bar';";
+   
+   private static final String FAILURE_1 = 
+   "class Foo {\n"+
+   "   let x = 1;\n"+
+   "}\n"+
+   "class Bar extends Foo {\n"+
+   "   let x = 11;\n"+
+   "}\n";
+   
+   @Bug
+   public void testDuplicateProperty() throws Exception {
+      //assertCompileSuccess(SUCCESS_1);
+      //assertCompileAndExecuteSuccess(SUCCESS_2);
+      assertCompileError(FAILURE_1, "Type 'default.Bar' has a duplicate property 'x' in /default.snap at line 4");
+   }
+         
+}
---
+++ snap-tree/src/main/java/org/snapscript/tree/define/MemberFieldReference.java
@@ -0,0 +1,33 @@
+package org.snapscript.tree.define;
+
+import org.snapscript.core.ModifierType;
+import org.snapscript.core.scope.Scope;
+import org.snapscript.core.type.Type;
+import org.snapscript.tree.NameReference;
+import org.snapscript.tree.literal.TextLiteral;
+
+public class MemberFieldReference {
+   
+   private final NameReference identifier;
+   
+   public MemberFieldReference(TextLiteral identifier) {
+      this.identifier = new NameReference(identifier);
+   }
+   
+   public String getName(Type type, int modifiers) throws Exception {
+      Scope scope = type.getScope();
+      String name = identifier.getName(scope);
+      Class real = type.getType();
+      
+      if(real == null) {
+         int order = type.getOrder();
+         
+         if(ModifierType.isPrivate(modifiers)) {
+            if(order > 0) {
+               return name; // private name should be unique, fix text templates
+            }
+         }
+      }
+      return name;
+   }
+}
