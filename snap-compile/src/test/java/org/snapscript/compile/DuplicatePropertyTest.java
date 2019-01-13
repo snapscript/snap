5571c318a8b02527a15471c1118dee356a8d5ddd
Authrus <authrus.root@gmail.com>
2019-01-13 01:26:30 +0000

    Add additional duplicate test

--- snap-compile/src/test/java/org/snapscript/compile/DuplicatePropertyTest.java
+++ snap-compile/src/test/java/org/snapscript/compile/DuplicatePropertyTest.java
@@ -29,6 +29,12 @@ public class DuplicatePropertyTest extends CompileTestCase {
    "println(new Bar().name);\n"+
    "assert new Bar().name == 'Bar';";
    
+   private static final String SUCCESS_3 = 
+   "import swing.JFrame;\n" +         
+   "class Game extends JFrame {\n"+
+   "   private height = 1;\n"+
+   "}\n";
+   
    private static final String FAILURE_1 = 
    "class Foo {\n"+
    "   let x = 1;\n"+
@@ -41,6 +47,7 @@ public class DuplicatePropertyTest extends CompileTestCase {
    public void testDuplicateProperty() throws Exception {
       //assertCompileSuccess(SUCCESS_1);
       //assertCompileAndExecuteSuccess(SUCCESS_2);
+      //assertCompileSuccess(SUCCESS_3);
       assertCompileError(FAILURE_1, "Type 'default.Bar' has a duplicate property 'x' in /default.snap at line 4");
    }
          
